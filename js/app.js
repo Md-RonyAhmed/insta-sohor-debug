5be767c987f21c8b949dcf4c3c42144277f4779c
Md. Rony <rony15-10527@diu.edu.bd>
2022-03-11 10:39:22 +0600

    first error solved

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ const isLiked = (id) => {
 };
 
 const addToLiked = (id) => {
-    likedPostsId.plus(id); 
+    likedPostsId.push(id); // first error solved
     showPosts(posts);
 };
 
@@ -85,7 +85,7 @@ const createPost = (post) => {
               <div class="post__footer">
                 <div class="post__buttons">
                   <button class="post__button" onclick="addToLiked(${post.id})">
-                  <i class="fa-solid fa-heart ${isLiked(post.id) && "text-danger"}"></i>
+                  <i class="fa-solid fa-heart ${isLiked(post.id) && 'text-danger'}"></i>
                     
                   </button>
                   <button class="post__button">
